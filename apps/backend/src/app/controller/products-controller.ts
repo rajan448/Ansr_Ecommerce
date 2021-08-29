4f00cf9e79ba2ce16362ee197a827c2378292ed2
rajan <rajank@dewsolutions.in>
2021-08-29 14:42:44 +0530

    Implement get product by id

--- apps/backend/src/app/controller/products-controller.ts
+++ apps/backend/src/app/controller/products-controller.ts
@@ -1,3 +1,4 @@
+import { Request, Response } from 'express';
 import { model } from 'mongoose';
 import { Product, productSchema } from '../model/product';
 
@@ -6,6 +7,18 @@ const ProductModel = model<Product>('products', productSchema);
 
 export const getProducts = (req, res) => {
     ProductModel.find({}, function (err, products) {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(products);
+        }
+    });
+};
+
+export const getProductsById = (req: Request , res: Response) => {
+    const uniq_id = req.params.id;
+
+    ProductModel.find({uniq_id}, function (err, products) {
         if (err) {
             res.status(500).send(err);
         } else {
--- apps/backend/src/app/routes/app-routes.ts
+++ apps/backend/src/app/routes/app-routes.ts
@@ -1,7 +1,8 @@
-import { getProducts } from "../controller/products-controller";
+import { getProducts, getProductsById } from "../controller/products-controller";
 
 const routes = (app) => {
     app.get('/api/products', getProducts);
+    app.get('/api/products/:id', getProductsById);
 }
 
 export default routes;
